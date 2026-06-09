206bbc431767a8fee042b9562a737f64cea18d50
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2026-06-09 09:30:01 -0500

    feat: metodo de pago

---
+++ src/main/groovy/gestionventas/Model/MetodoPago.java
@@ -0,0 +1,12 @@
+package gestionventas.Model;
+
+public enum MetodoPago {
+    EFECTIVO,
+    TARJETA_CREDITO,
+    TARJETA_DEBITO,
+    TRANSFERENCIA_BANCARIA,
+    NEQUI,
+    DAVIPLATA,
+    PAYPAL,
+    PSE
+}
