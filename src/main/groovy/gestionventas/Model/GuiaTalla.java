fbc3421a58c8b2eca72a3f05f8efceccf42b1a9f
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2026-06-09 09:50:20 -0500

    feat: guia talla

---
+++ src/main/groovy/gestionventas/Model/GuiaTalla.java
@@ -0,0 +1,31 @@
+package gestionventas.Model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Entity
+@Table(name = "guias_talla")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GuiaTalla {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idGuia;
+
+    @NotNull(message = "La medida de pecho es obligatoria")
+    private Double pecho;
+
+    @NotNull(message = "La medida de cintura es obligatoria")
+    private Double cintura;
+
+    @NotNull(message = "La medida de cadera es obligatoria")
+    private Double cadera;
+
+    @NotNull(message = "La medida de largo es obligatoria")
+    private Double largo;
+}
