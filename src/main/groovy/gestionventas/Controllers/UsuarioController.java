0e723eca60aa32cb86903c096cca0cbdaab95983
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2026-06-24 11:31:30 -0500

    feat: UsuarioController

---
+++ src/main/groovy/gestionventas/Controllers/UsuarioController.java
@@ -0,0 +1,83 @@
+package gestionventas.Controllers;
+
+import gestionventas.Dto.CrearUsuarioRequestDTO;
+import gestionventas.Dto.UsuarioResponseDTO;
+import gestionventas.Model.Rol;
+import gestionventas.Services.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public UsuarioResponseDTO crearUsuario(
+            @Valid @RequestBody CrearUsuarioRequestDTO dto) {
+
+        return usuarioService.crearUsuario(dto);
+    }
+
+    @GetMapping
+    public List<UsuarioResponseDTO> listarUsuarios() {
+
+        return usuarioService.listarUsuarios();
+    }
+
+    @GetMapping("/{id}")
+    public UsuarioResponseDTO buscarPorId(
+            @PathVariable Long id) {
+
+        return usuarioService.buscarPorId(id);
+    }
+
+    @GetMapping("/correo/{correo}")
+    public UsuarioResponseDTO buscarPorCorreo(
+            @PathVariable String correo) {
+
+        return usuarioService.buscarPorCorreo(correo);
+    }
+
+    @GetMapping("/rol/{rol}")
+    public List<UsuarioResponseDTO> buscarPorRol(
+            @PathVariable Rol rol) {
+
+        return usuarioService.buscarPorRol(rol);
+    }
+
+    @PutMapping("/{id}")
+    public UsuarioResponseDTO actualizarUsuario(
+            @PathVariable Long id,
+            @Valid @RequestBody CrearUsuarioRequestDTO dto) {
+
+        return usuarioService.actualizarUsuario(id, dto);
+    }
+
+    @PatchMapping("/{id}/activar")
+    public UsuarioResponseDTO activarUsuario(
+            @PathVariable Long id) {
+
+        return usuarioService.activarUsuario(id);
+    }
+
+    @PatchMapping("/{id}/desactivar")
+    public UsuarioResponseDTO desactivarUsuario(
+            @PathVariable Long id) {
+
+        return usuarioService.desactivarUsuario(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminarUsuario(
+            @PathVariable Long id) {
+
+        usuarioService.eliminarUsuario(id);
+    }
+
+}
