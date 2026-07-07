c4d625d36c1230099de003c7164a0d112c528986
nikoll <nikolls.ocampog@uqvirtual.edu.co>
2026-07-07 09:14:54 -0500

    feat: iniciarsesion

--- src/main/groovy/gestionventas/Config/SecurityConfig.java
+++ src/main/groovy/gestionventas/Config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
 
                         .requestMatchers(
                                 "/auth/**",
-                                "/usuarios/crear"
+                                "/usuarios/crear",
+                                "/usuarios/login"
                         ).permitAll()
 
                         .requestMatchers(
